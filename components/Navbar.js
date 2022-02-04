77535a9310f418aec81a139b8aef4176f77f059d
Bakhtiyor Khasanov <ota.76@mail.ru>
2022-02-04 17:18:39 +0500

    add home path to bred

--- components/Navbar.js
+++ components/Navbar.js
@@ -5,9 +5,12 @@ import { CodeIcon } from "@heroicons/react/solid";
 export default function Navbar() {
   return (
     <div className="pt-5 pb-2 mb-4 flex flex-col sm:flex-row items-center justify-between border-b-2 border-zinc-300">
-      <h2 className="text-2xl md:text-3xl font-bold text-zinc-700 hover:text-zinc-500">
-      <CodeIcon className="w-12 h-12 inline" /> Bakhtiyor <span className="font-thin">Khasanov</span>
-      </h2>
+      <Link href={'/'} passHref>
+        <h2 className="text-2xl md:text-3xl font-bold text-zinc-700 hover:text-zinc-500 cursor-pointer">
+          <CodeIcon className="w-12 h-12 inline" /> Bakhtiyor{" "}
+          <span className="font-thin">Khasanov</span>
+        </h2>
+      </Link>
       <div className="space-x-0">
         <Link href={"/"}>
           <a className="nav-link">Home</a>
